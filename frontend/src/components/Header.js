6514444b67d2ad1e5be53984720cd0debea29ff5
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-01-02 22:38:44 -0700

    Finished the detailed product view when the user clicks on an item they like

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,17 +6,17 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <LinkContainer  to='/'>
-                    <Navbar.Brand>Toys Shop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Toys Shop</Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                        <LinkContainer  to='/cart'>
-                            <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
                             </LinkContainer>
-                            <LinkContainer  to='/login'>
-                            <Nav.Link><i className="fas fa-user"></i> Login In</Nav.Link>
+                            <LinkContainer to='/login'>
+                                <Nav.Link><i className="fas fa-user"></i> Account</Nav.Link>
                             </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
--- frontend/src/views/ProductDetailedView.js
+++ frontend/src/views/ProductDetailedView.js
@@ -1,11 +1,72 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from '../components/Rating';
+import products from '../products';
 
-const ProductDetailedView = () => {
+const ProductDetailedView = ({ match }) => {
+    const item = products.find(p => p._id === match.params.id)
     return (
-        <div>
-            Product
-        </div>
+        <>
+        <Link className='btn btn-light my-2' to='/' style={{opacity: 0}}>
+            Back to Shopping
+        </Link>
+        <Row>
+            <Col md={6}>
+                <Image src={item.image} alt={item.name} fluid />
+            </Col>
+            <Col md={3}>
+                <ListGroup variant='flush'>
+                    <ListGroup.Item>
+                        <h3>{item.name}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating value={item.rating} text={`${item.numReviews} reviews`}/>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Price: ${item.price}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Overview: {item.description}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                Inventory Status:
+                                </Col>
+                                <Col style={item.countInStock > 0 ? {color: 'green',} : {color: 'red',}}>
+                                {item.countInStock > 0 ? 'In Stock': 'Out of Stock'}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                Price:
+                                </Col>
+                                <Col>
+                                <strong>${item.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+
+                        <ListGroup.Item>
+                            <Button className='w-100' type='button' disabled={item.countInStock === 0}>
+                                Add To Cart
+                            </Button>
+                        </ListGroup.Item>
+
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+        </>
     )
 }
-
 export default ProductDetailedView
