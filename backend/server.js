e431466091f028399c9c6b2f58d202149990cad8
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-01-03 00:14:47 -0700

    Finished a simple express backend setup of the GET API calls

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 const express = require('express')
+const products = require('./data/products')
 
 const app = express()
+
+app.get('/', (request, response) => {
+    response.send('The GET API is working...')
+})
+
+app.get('/api/products', (request, response) => {
+    response.json(products)
+})
+
+app.get('/api/products/:id', (request, response) => {
+    const item = products.find(i => i._id === request.params.id)
+    response.json(item)
+})
 
 app.listen(5000, console.log('Server running on port 5000'))
