b4e2797d90212c4923c06e5b23579592d7bcaca7
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-01-04 22:55:20 -0700

    Added the optional arguments

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI,)
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true, 
+            useNewUrlParser: true, 
+        })
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.error(`Error: ${error.message}`)
