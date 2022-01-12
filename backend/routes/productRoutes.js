a652c03cfac10efd2e81281c691a16953ab6b673
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-01-11 20:08:32 -0700

    Adding the controllers, for making the routes easier and shorter

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,28 +1,9 @@
 import express from 'express';
-import asyncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js'
+import { getProducts, getProductByID } from '../controllers/productController.js';
 
-// @description: Get all of the products in the database
-// @route: GET Request to the products
-//@acesss: Public-> meaning anyone can hit this route
-router.get('/', asyncHandler(async (request, response) => {
-    const products = await Product.find({});
-    response.json(products);
-}))
 
-// @description: Get one single item in the database
-// @route: GET Request to the product with their id's 
-//@acesss: Public-> meaning anyone can hit this route
-router.get('/:id', asyncHandler(async (request, response) => {
-    const item = await Product.findById(request.params.id)
-
-    if(item) {
-        response.json(item);
-    } else {
-        response.status(404);
-        throw new Error('Item not found')
-}}))
-
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductByID);
 
 export default router;
