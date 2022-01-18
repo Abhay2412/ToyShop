483a150dd6787349f9172ef32c8e0ffe809d43e7
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-01-17 21:25:36 -0700

    Finished the register view and be able to sign up with the reducers and actions added in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import HomeView from './views/HomeView';
 import ProductDetailedView from './views/ProductDetailedView';
 import CartView from './views/CartView';
 import SignInView from './views/SignInView';
+import RegisterView from './views/RegisterView';
 
 const App = () => { //Arrow function 
   return (
@@ -15,6 +16,7 @@ const App = () => { //Arrow function
     <main className="py-4">
       <Container>
       <Route path='/login' component={SignInView} />
+      <Route path='/register' component={RegisterView} />
       <Route path='/product/:id' component={ProductDetailedView} />
       <Route path='/cart/:id?' component={CartView} />
       <Route path='/' component={HomeView} exact />
