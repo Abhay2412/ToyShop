33f4b1a21c82d18c1396cf3a601025a2d245cfd9
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-02-03 10:55:10 -0700

    Finished the Shipping View and added the save address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartView from './views/CartView';
 import SignInView from './views/SignInView';
 import RegisterView from './views/RegisterView';
 import ProfileView from './views/ProfileView';
+import ShippingView from './views/ShippingView';
 
 const App = () => { //Arrow function 
   return (
@@ -16,6 +17,7 @@ const App = () => { //Arrow function
     <Header />
     <main className="py-4">
       <Container>
+      <Route path='/shipping' component={ShippingView} />
       <Route path='/login' component={SignInView} />
       <Route path='/register' component={RegisterView} />
       <Route path='/profile' component={ProfileView} />
