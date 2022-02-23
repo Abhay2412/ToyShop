59d4fc80c8630ed7de150b1e3949292398d90a71
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-02-23 16:01:58 -0700

    Able to create a product now still need to make it so it can be updated

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,5 +1,6 @@
 import { PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAILS_REQUEST, 
-    PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, } from "../constants/productConstants";
+    PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL,
+PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_RESET, } from "../constants/productConstants";
 
 export const productListReducer = (state = { products: [] }, action) => {
     switch(action.type) {
@@ -35,6 +36,21 @@ export const productDeleteReducer = (state = { }, action) => {
             return { loading: false, success:true };
         case PRODUCT_DELETE_FAIL:
             return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const productCreateReducer = (state = { }, action) => {
+    switch(action.type) {
+        case PRODUCT_CREATE_REQUEST:
+            return { loading: true };
+        case PRODUCT_CREATE_SUCCESS:
+            return { loading: false, success:true, product: action.payload };
+        case PRODUCT_CREATE_FAIL:
+            return { loading: false, error: action.payload };
+        case PRODUCT_CREATE_RESET:
+            return {};
         default:
             return state;
     }
--- frontend/src/views/ProductListView.js
+++ frontend/src/views/ProductListView.js
@@ -4,8 +4,8 @@ import { Table, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
-import { listProducts, deleteProduct } from '../actions/productActions';
-
+import { listProducts, deleteProduct, createProduct } from '../actions/productActions';
+import { PRODUCT_CREATE_RESET } from '../constants/productConstants';
 
 const ProductListView = ({ history, match }) => {
     const dispatch = useDispatch();
@@ -16,18 +16,27 @@ const ProductListView = ({ history, match }) => {
     const productDelete = useSelector((state) => state.productDelete);
     const { loading:loadingDelete, error:errorDelete, success:successDelete } = productDelete;
 
+    const productCreate = useSelector((state) => state.productCreate);
+    const { loading:loadingCreate, error:errorCreate, success:successCreate, product: createdProduct } = productCreate;
+
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
 
 
     useEffect(() => {
-        if(userInfo && userInfo.isAdmin) {
-            dispatch(listProducts());
+        dispatch({ type: PRODUCT_CREATE_RESET });
+
+        if(!userInfo.isAdmin) {
+            history.push('/login')
+        }
+
+        if(successCreate) {
+            history.push(`/admin/product/${createdProduct._id}/edit`);
         }
         else {
-            history.pushState('/login');
+            dispatch(listProducts());
         }
-    }, [dispatch, history, userInfo, successDelete]);
+    }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct]);
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure you want to delete this product?')) {
@@ -35,8 +44,8 @@ const ProductListView = ({ history, match }) => {
         }
     }
 
-    const createProductHandler = (product) => {
-        //Create Product Here
+    const createProductHandler = () => {
+        dispatch(createProduct());
     }
 
   return (
@@ -53,6 +62,8 @@ const ProductListView = ({ history, match }) => {
     </Row>
     {loadingDelete && <Loader/>}
     {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
+    {loadingCreate && <Loader/>}
+    {errorCreate && <Message variant='danger'>{errorCreate}</Message>}
     {loading ? <Loader/> : error ? <Message varaint='danger'>{error}</Message> : (
         <Table striped bordered hover responsive className='table-sm'>
             <thead>
