317782de479239ebd7847fbc96e0a19243cc180b
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-02-23 16:40:29 -0700

    The admin is now able to update the product successfully

--- frontend/src/views/ProductEditView.js
+++ frontend/src/views/ProductEditView.js
@@ -5,7 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import FormContainer from '../components/FormContainer';
-import { listProductDetails } from '../actions/productActions';
+import { listProductDetails, updateProduct } from '../actions/productActions';
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants';
 
 const ProductEditView = ({ match, history }) => {
     const productId = match.params.id;
@@ -24,8 +25,17 @@ const ProductEditView = ({ match, history }) => {
 
     const { loading, error, product } = productDetails;
 
+    const productUpdate = useSelector((state) => state.productUpdate);
+
+    const { loading:loadingUpdate, error:errorUpdate, success:successUpdate } = productUpdate;
+
 
     useEffect(() => {
+        if(successUpdate) {
+            dispatch({ tpye: PRODUCT_UPDATE_RESET });
+            history.push('/admin/productlist');
+        }
+        else {
         if(!product.name || product._id !== productId) {
             dispatch(listProductDetails(productId));
         }
@@ -38,12 +48,21 @@ const ProductEditView = ({ match, history }) => {
             setCountInStock(product.countInStock);
             setDescription(product.description);
         }
-        
-    }, [dispatch, productId, product, history]);
+    }
+    }, [dispatch, productId, product, history, successUpdate]);
 
     const submitHandler = (e) => {
         e.preventDefault();
-        //Update products 
+        dispatch(updateProduct({
+            _id: productId,
+            name, 
+            price,
+            image, 
+            brand, 
+            category,
+            description,
+            countInStock
+        }));
     }
 
     return ( 
@@ -53,6 +72,8 @@ const ProductEditView = ({ match, history }) => {
         </Link>
         <FormContainer>
         <h1>Edit Product</h1>
+        {loadingUpdate && <Loader/>}
+        {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
         {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
             <Form onSubmit={submitHandler}>
             <Form.Group controlId='name'>
