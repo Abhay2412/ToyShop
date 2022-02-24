56585d10d7ed774dc5e51e5dd085732abe23a390
Abhay2412 <57297897+Abhay2412@users.noreply.github.com>
2022-02-23 17:14:46 -0700

    Finish the uploading of the image for the products in the frontend

--- frontend/src/views/ProductEditView.js
+++ frontend/src/views/ProductEditView.js
@@ -53,6 +53,30 @@ const ProductEditView = ({ match, history }) => {
     }
     }, [dispatch, productId, product, history, successUpdate]);
 
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image', file);
+        setUploading(true);
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+        
+        const { data } = await axios.post('/api/upload', formData, config);
+        setImage(data);
+        setUploading(false);
+        }
+        catch (error) {
+            console.error(error);
+            setUploading(false);
+        }
+    }
+
+
     const submitHandler = (e) => {
         e.preventDefault();
         dispatch(updateProduct({
@@ -95,6 +119,8 @@ const ProductEditView = ({ match, history }) => {
                 <Form.Control type='text' placeholder='Enter image URL Link' value={image}
                 onChange={(e) => setImage(e.target.value)}>
                 </Form.Control>
+                <Form.File id='image-file' label='Choose File' custom onChange={uploadFileHandler}></Form.File>
+                {uploading && <Loader/>}
             </Form.Group>
             <Form.Group controlId='brand'>
                 <Form.Label>Brand</Form.Label>
